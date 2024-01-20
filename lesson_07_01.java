7d19a4dc52adc12e3efa07f5029079adb437bc57
ColotVS <kolotilov98@mail.ru>
2024-01-20 18:05:17 +0700

    Создание метода Урок 7, задача 1

---
+++ lesson_07_01.java
@@ -0,0 +1,14 @@
+public class lesson_07_01 {
+    //Создание нового медота. Метод — это группа команд, у которой есть уникальное имя
+    public static void main(String[] args)
+    {
+        printWiFi();
+        printWiFi();
+        printWiFi();
+    }
+    public static void printWiFi()
+    {
+        System.out.print("Wi-");
+        System.out.println("Fi");
+    }
+}
