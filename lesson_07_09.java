bf73100394a25feab951997f2ff0c14c72b6421a
ColotVS <kolotilov98@mail.ru>
2024-01-22 16:30:02 +0700

    Сигнатура метода Урок 7, задача 9

---
+++ lesson_07_09.java
@@ -0,0 +1,40 @@
+public class lesson_07_09 {
+    //Неплохо было бы иметь один метод для решения различных задач. У тебя есть возможность написать такой.
+    //Создай еще 9 методов universalMethod(). В сумме их должно получиться 10.
+    //Параметры, которые они должны принимать, придумай сам.
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+    public static void universalMethod(String name){
+
+    }
+    public static void universalMethod(String name, int num ){
+
+    }
+    public static void universalMethod(String name, double num ){
+
+    }
+    public static void universalMethod(int name, double num ){
+
+    }
+    public static void universalMethod(float name, double num ){
+
+    }
+    public static void universalMethod(int name, float num ){
+
+    }
+    public static void universalMethod(int name, String num ){
+
+    }
+    public static void universalMethod(boolean name, double num ){
+
+    }
+    public static void universalMethod(int [] name, double num )
+    {
+
+    }
+}
