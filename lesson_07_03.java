90598e78c066630a3eefdacd6ec3004bc939c151
ColotVS <kolotilov98@mail.ru>
2024-01-22 16:13:54 +0700

    Передача параметров Урок 7, задача 3

--- lesson_07_03.java
+++ lesson_07_03.java
@@ -1,14 +1,14 @@
 public class lesson_07_03 {
         //Передача параметров в метод
-        public static void printLines(String text, int count)
+        public static void printLines(String text, int count)//Объявили метод printLines с параметрами: String text, int count
         {
-            for (int i = 0; i < count; i++)
+            for (int i = 0; i < count; i++) //Метод выводит на экран count раз строку text
                 System.out.println(text);
         }
 
         public static void main(String[] args)
         {
-            printLines("Привет", 10);
+            printLines("Привет", 10); //Вызываем метод printLines с разными параметрами
             printLines("Пока", 20);
         }
 }
